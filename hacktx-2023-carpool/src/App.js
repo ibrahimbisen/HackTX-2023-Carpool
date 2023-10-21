5fb705782379234a8b40553714ea4250c233dec4
Kenta Fukuchi <75350032+RentaKenta@users.noreply.github.com>
2023-10-21 13:16:44 -0500

    clearing out unnecessary code

--- hacktx-2023-carpool/src/App.js
+++ hacktx-2023-carpool/src/App.js
@@ -1,16 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
 function App() {
   return (
-    <div className="App">
-      <p>
-        blah blah blah 
-        blah
-        blah blah
-        blah 
+    <div>
 
-      </p>
     </div>
   );
 }
