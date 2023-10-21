1a54255a70e1b8c8f41339b7709c9dbe10675536
Kenta Fukuchi <75350032+RentaKenta@users.noreply.github.com>
2023-10-21 17:59:32 -0500

    todo list test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
 
 const style = {
-  bg: `h-screen w-s p-4 bg-gradient-to-r from-[#2F80ED]`,
+  bg: `h-screen w-s p-4 bg-gradient-to-r from-[#2F80ED] to-[#1CB5E0]`,
 };
 
 function App() {
-  return <div className={style.bg}></div>;
+  return (
+  <div className={style.bg}>
+    <div className={style.container}>
+      <h3 classname={style.heading}>Todo App</h3>
+      <form className={style.form}>
+        <input className={style.input} type="text" placeholder="Add Todo" />
+        <button className={style.button}></button>
+      </form>
+    </div>
+  </div>
+  );
 }
 
 export default App;
